1c0e3d0113916ad11f3cb575d06d0339bcb8b17c
Abba Bawa <abbabawa6@gmail.com>
2023-05-16 20:47:45 +0100

    auth service worked on

---
+++ auth-service/models/user.ts
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import bycrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from 'crypto';
+import { model, Schema, Model, Document } from 'mongoose';
+
+//declare user type
+export interface IUser extends Document {
+    getSignedToken():string;
+    matchPassword(password: string): boolean | PromiseLike<boolean>;
+    username:string;
+    password:string;
+    email:string;
+
+}
+// define user schema
+const UserSchema: Schema = new Schema({
+    username: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, "Can't be blank"],
+        index: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+        minlength:  [8, "Please use minimum of 8 characters"],
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        required: [true, "Can't be blank"],
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please use a valid address'],
+        unique:true,
+        index:true
+    },
+    account_balance: {
+        type: Number
+    },
+
+    active: { type: Boolean, default: true }
+});
+
+UserSchema.pre<IUser>("save", async function (next: any) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bycrypt.genSalt(10);
+    this.password = bycrypt.hashSync(this.password, 10);
+    next();
+});
+
+UserSchema.methods.matchPassword= async function (password:string) {
+    return await bycrypt.compare(password,this.password)   
+}
+UserSchema.methods.getSignedToken= function (password:string) {
+    return jwt.sign({id:this._id},process.env.JWT_SECRET!,{
+        expiresIn:process.env.JWT_EXPIRE
+    })   
+}
+
+
+export const User:Model<IUser> = model("User", UserSchema);
---
+++ auth-service/routes/auth.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+
+//import controllers
+const { register, login } = require("../controllers/auth");
+
+//routes
+router.route("/register").post(register);
+router.route("/login").post(login);
+
+module.exports = router;
