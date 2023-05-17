0fff21f4aeffab28f6b462b3ec5f5dd03a0a9c3d
Abba Bawa <abbabawa6@gmail.com>
2023-05-17 14:09:57 +0100

    access token created on login and functionality to check access token validity added

--- auth-service/controllers/auth.ts
+++ auth-service/controllers/auth.ts
@@ -1,6 +1,10 @@
 import { Response, Request } from "express";
 import { IUser, User } from "../models/user";
 
+const crypto = require('crypto');
+var axios = require('axios');
+const jwtSecret = process.env.JWT_SECRET, jwt = require('jsonwebtoken');
+
 exports.register = async (req: Request, res: Response, next: any) => {
   const { username, email, password } = req.body;
   try {
@@ -9,6 +13,7 @@ exports.register = async (req: Request, res: Response, next: any) => {
       email,
       password,
     });
+    let token = jwt.sign(req.body, jwtSecret, {expiresIn: "24h"});
     res.status(200).send(user);
     //sendToken(user,201,res)
   } catch (error: any) {
@@ -34,10 +39,12 @@ exports.login = async (req: Request, res: Response, next: any) => {
       return next(new ErrorResponse("Invalid Credentials", 401));
     }
     const isMatch: boolean = await user.matchPassword(password);
+    let token = jwt.sign({...user.toJSON()}, jwtSecret, {expiresIn: "24h"});
+    let userData = {...user.toJSON(), token}
     if (!isMatch) {
       return next(new ErrorResponse("Invalid Credentials", 401));
     }
-    res.status(200).send(user);
+    res.status(200).send(userData);
     //res.send({user,200,res})
   } catch (error: any) {
     return next(new ErrorResponse(error.message, 500));
@@ -64,4 +71,59 @@ exports.getUserByEmail = async (req: Request, res: Response, next: any) => {
   } catch (error: any) {
     return next(new ErrorResponse(error.message, 500));
   }
+};
+
+exports.verifyToken = (req, res, next) => {
+    if (req.headers['authorization']) {
+        try {
+            let authorization = req.headers['authorization'].split(' ');
+            if (authorization[0] !== 'Bearer') {
+                res.header("Access-Control-Allow-Origin", "true");
+                return res.status(401).send(
+                    {
+                        status: false,
+                        message: "",
+                        errors: ["Authorization header missing"]
+                    }
+                );
+            } else {
+                req.jwt = jwt.verify(authorization[1], jwtSecret);
+                // return next();
+                return res.status(401).send(
+                    {
+                        status: true,
+                        message: "Token is valid",
+                        errors: []
+                    }
+                );
+            }
+
+        } catch (err) {console.log(err, err.message)
+            if(err.message === "jwt expired"){
+                return res.status(403).send(
+                    {
+                        status: false,
+                        type: "expired_token",
+                        message: "Expired AccessToken",
+                        errors: ["Internal server error"]
+                    }
+                );
+            }
+            return res.status(403).send(
+                {
+                    status: false,
+                    message: "",
+                    errors: ["Internal server error"]
+                }
+            );
+        }
+    } else {
+        return res.status(401).send(
+            {
+                status: false,
+                message: "Missing authorization header",
+                errors: ["User not logged in"]
+            }
+        );
+    }
 };
--- auth-service/routes/auth.ts
+++ auth-service/routes/auth.ts
@@ -2,11 +2,12 @@ import express from "express";
 const router = express.Router();
 
 //import controllers
-const { register, login, getUserByEmail } = require("../controllers/auth");
+const { register, login, getUserByEmail, verifyToken } = require("../controllers/auth");
 
 //routes
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/user/:email").get(getUserByEmail);
+router.route("/verify-token").post(verifyToken);
 
 module.exports = router;
